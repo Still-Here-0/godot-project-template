03a6bad025b52847fc27f1088a4d33ef1a15ddf5
Bruno Videira Pinho <brunovp.21@gmail.com>
2026-05-03 16:57:06 -0300

    Changing bash files

--- remove_gitkeep.bash
+++ remove_gitkeep.bash
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Remove all .gitkeep files from the project, excluding dot-folders like .git and .godot.
 find . -type d -not -path './.*' -exec find {} -maxdepth 1 -type f -name '.gitkeep' -delete \;
