b05b01d5d70dcefeedb05a46e49429cd74e3bcb2
Bruno Videira Pinho <brunovp.21@gmail.com>
2026-05-03 17:05:06 -0300

    Adding a init logic to godot template

--- init_new_project.bash
+++ init_new_project.bash
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+cd "$script_dir"
+
 find . -type d -not -path './.*' -exec find {} -maxdepth 1 -type f -name '.gitkeep' -delete \;
+
+if [[ -d .git ]]; then
+	rm -rf .git
+fi
+
+git init
